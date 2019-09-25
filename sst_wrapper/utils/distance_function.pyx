d7fc55d02dc58e502a3a827b05c9b664d19266c8
jacobjohnson-bc <c_johnson@braincorporation.com>
2019-09-25 16:40:53 -0700

    6-dim euclidean distance

--- sst_wrapper/utils/distance_function.pyx
+++ sst_wrapper/utils/distance_function.pyx
@@ -14,4 +14,4 @@ class TriStateDistance(_sst_module.IDistance):
     Custom distance function for Tricycle robot.
     """
     def distance(self,double[:] point1,double[:] point2):
-        return ((point1[0]-point2[0])**2 +(point1[1]-point2[1])**2+ normalize_angle(point1[2]-point2[2])**2 +normalize_angle(point1[5]-point2[5])**2)**0.5
+        return ((point1[0]-point2[0])**2 +(point1[1]-point2[1])**2+ normalize_angle(point1[2]-point2[2])**2 +(point1[3]-point2[3])**2+(point1[4]-point2[4])**2+normalize_angle(point1[5]-point2[5])**2)**0.5
