70cd3ff08b4dea850fbfef41ac92429d8a397f60
jacobjohnson-bc <c_johnson@braincorporation.com>
2019-09-25 11:47:29 -0700

    updated distance function to absolute values

--- sst_wrapper/utils/distance_function.pyx
+++ sst_wrapper/utils/distance_function.pyx
@@ -14,4 +14,5 @@ class TriStateDistance(_sst_module.IDistance):
     Custom distance function for Tricycle robot.
     """
     def distance(self,double[:] point1,double[:] point2):
-        return ( (point1[0]-point2[0])**2 +(point1[1]-point2[1])**2+normalize_angle(point1[2]-point2[2])**2+ normalize_angle(point1[5]-point2[5])**2)**0.5
+        cdef double distance = ((point1[0]-point2[0])**2 +(point1[1]-point2[1])**2)**0.5
+        return distance + abs(normalize_angle(point1[2]-point2[2]))+ abs(normalize_angle(point1[5]-point2[5]))
