db1f9b7a5a74ee09d2b7079bcb432cd372318897
jacobjohnson-bc <c_johnson@braincorporation.com>
2019-09-27 13:54:15 -0700

    corrected PEP8 errors

--- sst_wrapper/envs/gym_sst_wrapper.py
+++ sst_wrapper/envs/gym_sst_wrapper.py
@@ -6,7 +6,7 @@ from __future__ import division
 from sparse_rrt.systems.system_interface import BaseSystem
 
 
-class bc_sst_wrapper(BaseSystem):# pylint: disable=invalid-name
+class bc_sst_wrapper(BaseSystem):  # pylint: disable=invalid-name
     """
     A class to wrap a gym environment to facilitate planning using sparse RRT
     """
