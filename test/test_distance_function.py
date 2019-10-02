de006dbf84c282d8f0b8a61eeffd8040c01b0894
jacobjohnson-bc <c_johnson@braincorporation.com>
2019-10-02 09:45:11 -0700

    updated test

--- test/test_distance_function.py
+++ test/test_distance_function.py
@@ -2,11 +2,19 @@
 A test to check that the distance function returns the correct cost function.
 """
 from sst_wrapper.utils.distance_function import TriStateDistance
+from sst_wrapper.utils.distance_function import DiffStateDistance
 import numpy as np
 
 
-def test_distance():
+def test_tri_distance():
     distance_obj = TriStateDistance()
+    assert (distance_obj.distance(np.array([0, 0, 0, 0, 0, 0], dtype=np.float),
+                                  np.array([1, 1, 1, 1, 1, 1],
+                                           dtype=np.float)) == 2)
+
+
+def test_diff_distance():
+    distance_obj = DiffStateDistance()
     assert (distance_obj.distance(
         np.array([0, 0, 0, 0, 0, 0], dtype=np.float),
-        np.array([1, 1, 1, 1, 1, 1], dtype=np.float)) == 2)
+        np.array([1, 1, 1, 1, 1, 1], dtype=np.float)) == 1.7320508075688772)
